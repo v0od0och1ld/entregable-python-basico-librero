1fe2490a25e1599480399141250466ebd376d633
v0od0och1ld <pablo@pirri.com.ar>
2023-12-10 23:05:19 -0300

    231210 CRUD editorial terminado

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def crear_tabla_libros():
     """
     cursor.execute(sql)
     conn.commit()
+
 def crear_tabla_categorias():   
     conn = conexion()
     cursor = conn.cursor() 
@@ -32,6 +33,24 @@ def crear_tabla_categorias():
     """
     cursor.execute(sql1)
     conn.commit()
+
+def agregar_generos_literarios():
+    conn = conexion()
+    cursor = conn.cursor()
+
+    generos = ["Novela", "Poesía", "Drama", "Ensayo", "Ciencia ficción", "Fantasía", "Misterio", "Terror", "Aventura"]
+
+    cursor.execute("SELECT categoria FROM categorias")
+    categorias_existentes = cursor.fetchall()
+    categorias_existentes = [cat[0] for cat in categorias_existentes]
+        
+    for genero in generos:
+        if genero not in categorias_existentes:
+            sql = "INSERT INTO categorias (categoria) VALUES (?)"
+            cursor.execute(sql, (genero,))
+    
+    conn.commit()
+
 
 def crear_tabla_autores():
     conn = conexion()
@@ -59,6 +78,11 @@ try:
     crear_tabla_libros()    
 except:
     print("Hay un error en la creación de la tabla de libros o la misma ya fue creada anteriormente")
+try:
+    conexion()
+    agregar_generos_literarios()    
+except:
+    print("Hay un error en el agregado de los géneros literarios o los mismos ya fueron creados anteriormente")
 
 try:
     conexion()
@@ -144,10 +168,14 @@ def nueva_categoria():
         categoria = entry_categoria.get()
         resultado = buscar_categoria(categoria)
         if resultado:            
-            label_aviso.config(text="Categoría Existente", fg="red")            
+            label_aviso.config(text="Categoría Existente", fg="red")
+            showerror("Error", "Categoría Existente") 
+            top.after(0, lambda: top.focus_force())           
         else:            
             guardar_categoria(categoria)
-            label_aviso.config(text=f"Categoría '{categoria}' Guardada", fg="green")            
+            label_aviso.config(text=f"Categoría '{categoria}' Guardada", fg="green")
+            showinfo("Guardado", "Categoría guardada") 
+            top.after(0, lambda: top.focus_force())           
             entry_categoria.delete(0, 'end') 
 
     top = Toplevel()
@@ -173,11 +201,14 @@ def modificar_categoria():
         categoriamod = entry_nombre.get()        
         resultado = guardar_mod_categoria(nombre, categoriamod)
         if resultado:            
-            label_aviso.config(text="Modificación exitosa", fg="Green")       
+            label_aviso.config(text="Modificación exitosa", fg="Green")
+            showinfo("Modificar", "Categoría modificada") 
             top.after(1000, lambda: top.destroy())
             top.after(1000, lambda: root.focus_force())     
         else:            
             label_aviso.config(text=f"Error al modificar la categoría", fg="red")            
+            showerror("Error", "Error al modificar la categoría")
+            top.after(0, lambda: top.focus_force())
             
 
     #funcion para pasar texto al combobox
@@ -230,11 +261,14 @@ def eliminar_categoria():
         #categoriamod = entry_nombre.get()        
         resultado = borrar_categoria(nombre)
         if resultado:            
-            label_aviso.config(text="Borrado exitosa", fg="Green")       
+            label_aviso.config(text="Borrado exitosa", fg="Green")  
+            showinfo("Borrado", "Borrado Exitoso")
             top.after(1000, lambda: top.destroy())
             top.after(1000, lambda: root.focus_force())     
         else:            
-            label_aviso.config(text=f"Error al borrar la categoría", fg="red")            
+            label_aviso.config(text=f"Error al borrar la categoría", fg="red") 
+            showerror("Error", "Error al borrar la categoría")
+            top.after(0, lambda: top.focus_force())            
             
 
     #funcion para pasar la variable del combobox
@@ -250,12 +284,12 @@ def eliminar_categoria():
     ####Combobox 
     categorias = buscar_categorias()        
     combo = ttk.Combobox(top, values=categorias)
-    combo.pack()
+    combo.pack(pady=10)
     combo.bind("<<ComboboxSelected>>", seleccionar_item)
     ####Combobox   
     
     btn_borrar = Button(top, text="Borrar", command=lambda: eliminar_categoriain(nombre))
-    btn_borrar.pack()
+    btn_borrar.pack(pady=10)
     
     #### Label de notificación de mensajes
     label_aviso = Label(top)  
@@ -266,14 +300,195 @@ def eliminar_categoria():
 ################## CRUD CATEGORIA  ################
 ##################                 ################
 
-def nueva_editorial():
-    pass
+##################                 ################     
+################## CRUD EDITORIAL  ################
+##################                 ################
+def buscar_editorial(nombre_editorial):
+    conn = conexion()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM editoriales WHERE editorial = ?"
 
+    cursor.execute(sql, (nombre_editorial,))
+    resultado = cursor.fetchone()
+
+    return resultado
+
+def buscar_editoriales():
+    conn = conexion()
+    cursor = conn.cursor()
+    sql = "SELECT * FROM editoriales order by editorial asc"
+
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+
+    return resultado
+    
+    
+    
+# Graba en la bd la nueva editorial
+def guardar_editorial(nombre_editorial):
+    conn = conexion()
+    cursor = conn.cursor()
+    sql = "INSERT INTO editoriales (editorial) VALUES(?)"
+    try:
+        cursor.execute(sql, (nombre_editorial,))
+        print(f"Categoría guardada: {nombre_editorial}")
+    except:
+        print(f"Error al guardar la editorial {nombre_editorial}")
+    conn.commit()
+
+
+def guardar_mod_editorial(nombre, editorialmod):
+    conn = conexion()
+    cursor = conn.cursor()
+    sql = "UPDATE editoriales SET editorial = ? WHERE editorial = ?"
+    try:       
+        cursor.execute(sql, (editorialmod, nombre))
+        conn.commit()
+        
+        return 1        
+    except:
+        conn.close()  
+
+
+# ventana para agregar una nueva editorial
+def nueva_editorial():    
+    def guardar_editorialin():
+        editorial = entry_editorial.get()
+        resultado = buscar_editorial(editorial)
+        if resultado:            
+            label_aviso.config(text="Editorial Existente", fg="red")
+            showerror("Error", "Editorial Existente") 
+            top.after(0, lambda: top.focus_force())           
+        else:            
+            guardar_editorial(editorial)
+            label_aviso.config(text=f"Editorial '{editorial}' Guardada", fg="green")
+            showinfo("Guardado", "Editorial guardada") 
+            top.after(0, lambda: top.focus_force())           
+            entry_editorial.delete(0, 'end') 
+
+    top = Toplevel()
+    top.title("Nueva Editorial")
+    top.geometry("300x150")
+    
+
+    label_editorial = Label(top, text="Ingrese nueva Editorial:")
+    label_editorial.pack()
+
+    entry_editorial = Entry(top)
+    entry_editorial.pack()
+
+    btn_guardar = Button(top, text="Guardar", command=guardar_editorialin)
+    btn_guardar.pack()
+    label_aviso = Label(top)  
+    label_aviso.pack()  
+
+#Ventana para modificar editorial
 def modificar_editorial():
-    pass
+    nombre = None
+    def guardar_modeditorialin(nombre):
+        editorialmod = entry_nombre.get()        
+        resultado = guardar_mod_editorial(nombre, editorialmod)
+        if resultado:            
+            label_aviso.config(text="Modificación exitosa", fg="Green")
+            showinfo("Modificar", "Editorial modificada") 
+            top.after(1000, lambda: top.destroy())
+            top.after(1000, lambda: root.focus_force())     
+        else:            
+            label_aviso.config(text=f"Error al modificar la Editorial", fg="red")            
+            showerror("Error", "Error al modificar la Editorial")
+            top.after(0, lambda: top.focus_force())
+            
+
+    #funcion para pasar texto al combobox
+    def seleccionar_item(event):
+        nonlocal nombre
+        valor_seleccionado = combo.get()
+        nombre = valor_seleccionado.split(' ')[1]
+        entry_nombre.delete(0, 'end')
+        entry_nombre.insert(0, nombre)    
+
+    top = Toplevel()
+    top.title("Modificar Categoría")
+    top.geometry("300x150")
+    
+    ####Combobox 
+    editoriales = buscar_editoriales()        
+    combo = ttk.Combobox(top, values=editoriales)
+    combo.pack()
+    combo.bind("<<ComboboxSelected>>", seleccionar_item)
+    ####Combobox
+   
+    entry_nombre = ttk.Entry(top)
+    entry_nombre.pack(pady=10)
+    
+    btn_guardar = Button(top, text="Guardar modificación", command=lambda: guardar_modeditorialin(nombre))
+    btn_guardar.pack()
+    
+    #### Label de notificación de mensajes
+    label_aviso = Label(top)  
+    label_aviso.pack()  
+    #### Label de notificación de mensajes
+
+
+def borrar_editorial(nombre):
+    conn = conexion()
+    cursor = conn.cursor()
+    sql = "DELETE FROM editoriales WHERE editorial = ?"
+        
+    try:       
+        cursor.execute(sql, (nombre,))
+        conn.commit()
+        conn.close()        
+        return 1        
+    except:
+        conn.close()  
 
 def eliminar_editorial():
-    pass
+    nombre = None
+    def eliminar_editorialin(nombre):
+        #categoriamod = entry_nombre.get()        
+        resultado = borrar_editorial(nombre)
+        if resultado:            
+            label_aviso.config(text="Borrado exitosa", fg="Green")  
+            showinfo("Borrado", "Borrado Exitoso")
+            top.after(1000, lambda: top.destroy())
+            top.after(1000, lambda: root.focus_force())     
+        else:            
+            label_aviso.config(text=f"Error al borrar la editorial", fg="red") 
+            showerror("Error", "Error al borrar la editorial")
+            top.after(0, lambda: top.focus_force())            
+            
+
+    #funcion para pasar la variable del combobox
+    def seleccionar_item(event):
+        nonlocal nombre
+        valor_seleccionado = combo.get()
+        nombre = valor_seleccionado.split(' ')[1]          
+
+    top = Toplevel()
+    top.title("Borrar Editorial")
+    top.geometry("300x150")
+    
+    ####Combobox 
+    editoriales = buscar_editoriales()        
+    combo = ttk.Combobox(top, values=editoriales)
+    combo.pack(pady=10)
+    combo.bind("<<ComboboxSelected>>", seleccionar_item)
+    ####Combobox   
+    
+    btn_borrar = Button(top, text="Borrar", command=lambda: eliminar_editorialin(nombre))
+    btn_borrar.pack(pady=10)
+    
+    #### Label de notificación de mensajes
+    label_aviso = Label(top)  
+    label_aviso.pack()  
+    #### Label de notificación de mensajes
+
+##################                  ################     
+################## CRUD EDITORIAL   ################
+##################                  ################
+
 
 def nuevo_autor():
     pass
